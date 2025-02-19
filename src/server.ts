53e777c643a4405fd2a8394bc0db127a4594fab7
MaximVanchev <maxim.van.mv@gmail.com>
2025-02-19 07:44:00 +0200

    Adding Routing with Decorators.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import http from "http";
 import express from "express";
 import "./config/logging";
+import "reflect-metadata";
 
 import { loggingHandler } from "./middleware/loggingHandler";
 import { corsHandler } from "./middleware/corsHandler";
 import { routeNotFound } from "./middleware/routeNotFound";
 import { SERVER, SERVER_HOSTNAME, SERVER_PORT } from "./config/config";
+import MainController from "./controllers/main";
+import { defineRoutes } from "./modules/routes";
 
 export const application = express();
 export let httpServer: ReturnType<typeof http.createServer>;
@@ -26,9 +29,7 @@ export const Main = () => {
   logging.log("----------------------------------------");
   logging.log("Define Controller Routing");
   logging.log("----------------------------------------");
-  application.get("/main/test", (req, res, next) => {
-    res.status(200).json({ hello: "world!" });
-  });
+  defineRoutes([MainController], application);
 
   logging.log("----------------------------------------");
   logging.log("Define Routing Error");
